62f6d9a3143b44183867d00457e8375780714906
huxiaoping <huxiaoping@godeepsite.com>
2020-07-26 17:01:27 +0800

    Initial version of gateway generated by JHipster-6.10.1

---
+++ src/main/java/hu/xiaoping/bestshop/gateway/security/SpringSecurityAuditorAware.java
@@ -0,0 +1,20 @@
+package hu.xiaoping.bestshop.gateway.security;
+
+import hu.xiaoping.bestshop.gateway.config.Constants;
+
+import java.util.Optional;
+
+import org.springframework.data.domain.AuditorAware;
+import org.springframework.stereotype.Component;
+
+/**
+ * Implementation of {@link AuditorAware} based on Spring Security.
+ */
+@Component
+public class SpringSecurityAuditorAware implements AuditorAware<String> {
+
+    @Override
+    public Optional<String> getCurrentAuditor() {
+        return Optional.of(SecurityUtils.getCurrentUserLogin().orElse(Constants.SYSTEM_ACCOUNT));
+    }
+}
---
+++ src/main/java/hu/xiaoping/bestshop/gateway/security/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Spring Security configuration.
+ */
+package hu.xiaoping.bestshop.gateway.security;
